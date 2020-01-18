911e6ec00e409476324ed53df1037dfc2907f3cb
Kevin Moe Myint Myat <mmm.myintmyat@gmail.com>
2020-01-18 14:36:23 +0800

    delete autoforever ps1 file

--- autoforever.ps1
+++
@@ -1,7 +0,0 @@
-
-   $nodePath = 'C:\Program Files\nodejs\npm.cmd'
-   $install = 'install --save --prefix $PSScriptRoot'
-   Start-Process -FilePath $nodePath -verb runAs -ArgumentList $install -Wait;
-   $foreverPath = 'C:\Users\e_swen\AppData\Roaming\npm\forever.cmd'
-   $start = "--uid mesftp-server --append -w start server.js";
-   Start-Process -FilePath $foreverPath -verb runAs -ArgumentList $start -Wait;
